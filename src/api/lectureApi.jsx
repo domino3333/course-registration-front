120524f75418234f49c374863daffc9ce7398957
MINO\User <busster@naver.com>
2026-04-24 08:33:25 +0900

    commit test

--- src/api/lectureApi.jsx
+++ src/api/lectureApi.jsx
@@ -7,4 +7,6 @@ export const getLectureList = async () => {
     const result = await axios.get(`${ApiHost}/api/lecture`);
     console.log('getLectureList함수 데이터:',result);
     return result.data;
+
+    
 }
