bb8fde519f385c040951bb167f999364fc37f560
MINO\User <busster@naver.com>
2026-04-26 11:50:55 +0900

    버튼 컴포넌트 추가

---
+++ src/components/button/GreenMiniBtn.jsx
@@ -0,0 +1,17 @@
+
+import "../../css/components/GreenMiniBtn.css"
+
+const GreenMiniBtn = ({ text }) => {
+
+
+    return (
+        <>
+            <button type="button">
+                {text}
+            </button>
+        </>
+    )
+
+}
+
+export default GreenMiniBtn;
