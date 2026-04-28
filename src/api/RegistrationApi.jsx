f9a223eda56175aec72e5eac35953d2340f08856
MINO\User <busster@naver.com>
2026-04-28 15:21:05 +0900

    api 형식 통일하기

--- src/api/RegistrationApi.jsx
+++ src/api/RegistrationApi.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { ApiHost, token } from "./ApiHost";
 
-const apiPrefix = 'api/registration';
+const apiPrefix = '/api/registration';
 
 //수강 '신청' 버튼 클릭
 export const enroll = async (lectureNo) => {
 
-    const result = await axios.post(`${ApiHost}/${apiPrefix}${lectureNo}`,null,{
+    const result = await axios.post(`${ApiHost}${apiPrefix}/${lectureNo}`,null,{
         headers:{
             Authorization:`Bearer ${token}`
         }
@@ -19,7 +19,7 @@ export const enroll = async (lectureNo) => {
 // 신청한 강의 목록 불러오기
 export const getRegistrationList = async () => {
 
-    const result = await axios.get(`${ApiHost}/${apiPrefix}`,{
+    const result = await axios.get(`${ApiHost}${apiPrefix}`,{
         headers:{
             Authorization:`Bearer ${token}`
         }
@@ -35,7 +35,7 @@ export const getRegistrationList = async () => {
 //신청한 강의 삭제(수강취소)
 export const cancelLecture = async(registrationNo) =>{
 
-    const result = axios.delete(`${ApiHost}/${apiPrefix}/${registrationNo}`,
+    const result = axios.delete(`${ApiHost}${apiPrefix}/${registrationNo}`,
         {
             headers:{
                 Authorization:`Bearer ${token}`
